b67545926506935f67bedb6469e060a3d9bc7ae3
Brokkoly <brendankrull@gmail.com>
2020-06-14 11:38:04 -0500

    Commit before branching for database stuff

--- brokkoly_bot_create_database.py
+++ brokkoly_bot_create_database.py
@@ -10,27 +10,43 @@ except:
     quit()
 
 try:
+    cursor = None
 
     # drop_server_list(conn)
     # drop_command_list(conn)
     # create_tables(conn)
 
-    add_command(conn, 0, "test", "test message")
-    print(select_all(conn))
-    print("Found Message: ", get_message(conn, 0, "test"))
+    # add_command(conn, 0, "test", "test message")
 
+    insert_query = """ INSERT INTO COMMAND_LIST (server_id, command_string, entry_value) VALUES (%s,%s,%s)"""
+    values = (0, "test", "test message")
+    add_command(conn, 0, 'test', 'test_message')
+    # cursor = conn.cursor()
+
+    # cursor.execute(insert_query,values)
     conn.commit()
 
-    # cur = conn.cursor()
-    # rows = cur.fetchall()
+    # count = cursor.rowcount
+
+    # print(count, "Record successfully inserted")
+    # print(select_all(conn))
+    print("Found Message: ", get_message(conn, 0, "test"))
+    # select_query = "SELECT * FROM COMMAND_LIST"
+    # cursor.execute(select_query)
+
+    # rows = cursor.fetchall()
     # for row in rows:
-    #     print
-    #     "   ", row[1][1]
-    # cur.close()
+    # print("Col0 = ",row[0])
+    # print("Col1 = ", row[1])
+    # print("Col2 = ", row[2])
+    # print("Col3 = ", row[3], "\n")
+    # cursor.close()
 except (Exception, psycopg2.DatabaseError) as error:
     print("Error while accessing table", error)
 
 finally:
     if (conn):
+        if (cursor):
+            cursor.close()
         conn.close()
         print("PostgreSCQL connection is closed")
--- brokkoly_bot_database.py
+++ brokkoly_bot_database.py
@@ -7,7 +7,7 @@ import os
 
 def add_command(conn, server_id, command, message):
     """
-
+    Adds a new command to the bot
     :param conn:
     :param server_id:
     :param command:
@@ -15,13 +15,19 @@ def add_command(conn, server_id, command, message):
     :return:
     """
     # TODO check if the entry already exists
-    send_query(conn, """
-    INSERT INTO COMMAND_LIST(server_id,command_string,entry_value)
-    VALUES
-        (%d,\'%s\',\'%s\');
-    """ % (server_id, command, message)
-               )
-    return
+    cursor = conn.cursor()
+    send_query(cursor, """
+        SELECT * FROM COMMAND_LIST
+        WHERE server_id = %s
+            AND command_string = %s
+            AND entry_value = %s;""",
+               (server_id, command, message))
+    if (cursor.rowcount > 0):
+        print("That command already exists!\n", "Count=%d" % (cursor.rowcount))
+        return False
+    send_query(cursor, """ INSERT INTO COMMAND_LIST (server_id, command_string, entry_value) VALUES (%s,%s,%s)""",
+               (server_id, command, message))
+    return True
 
 
 def select_all(conn):
@@ -37,8 +43,8 @@ def get_message(conn, server_id, command):
     :param command:
     :return:
     """
-
-    message = send_query(conn, """
+    cursor = conn.cursor()
+    send_query(cursor, """
     SELECT entry_value
     FROM COMMAND_LIST
     WHERE server_id = %d
@@ -46,7 +52,9 @@ def get_message(conn, server_id, command):
     ORDER BY RANDOM()
     LIMIT 1;
     """ % (server_id, command))
-    return message
+    message = cursor.fetchone()
+    print(message[0])
+    return message[0]
 
 
 def create_tables(conn):
@@ -84,15 +92,13 @@ def drop_server_list(conn):
                )
 
 
-def send_query(conn, command):
+def send_query(cur, command, args=None):
     print(command)
-    cur = conn.cursor()
     try:
-        message = cur.execute(command)
+        if (args):
+            cur.execute(command, args)
+        else:
+            cur.execute(command)
+
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error while executing command:\n", error)
-        message = ""
-    finally:
-        if (cur):
-            cur.close()
-        return message
